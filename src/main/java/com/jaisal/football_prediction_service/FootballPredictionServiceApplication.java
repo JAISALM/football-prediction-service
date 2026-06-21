36b9041ca7b3137d4654390a7969a3cd0521f2cd
Jaisal Mohammed <jaisal.3.m@gmail.com>
2026-06-21 21:42:59 +0530

    Initial project setup

---
+++ src/main/java/com/jaisal/football_prediction_service/FootballPredictionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jaisal.football_prediction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballPredictionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballPredictionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaisal/football_prediction_service/FootballPredictionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaisal.football_prediction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballPredictionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
